ba8341a8e65b26917059d3f41ef184017709d2b6
copsgit <github@copsking.com>
2022-12-16 20:47:41 -0500

    contract init with base functions

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -11,8 +11,71 @@ contract contractCF {
         uint256 amountCollected;
         string image;
         address[] donators;
-        uint256 donations;
+        uint256[] donations;
     }
     mapping(uint256 => Campaign) public campaigns;
-    uint256 public campaignCount = 0;
+
+    uint256 public numberOfCampaigns = 0;
+
+    function createCampaign(
+        address _owner,
+        string memory _title,
+        string memory _description,
+        uint256 _target,
+        uint256 _deadline,
+        string memory _image
+    ) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        //is everything ok with this?
+        require(campaign.deadline > block.timestamp, "Campaign has expired");
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
+    }
+
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent,) = payable(campaign.owner).call{value : amount}("");
+
+        if(sent) {
+            campaign.amountCollected += amount;
+        }
+    }
+
+    function getDonators(uint256 _campaignId)
+        public
+        view
+        returns (address[] memory, uint256[] memory)
+    {
+        return (
+            campaigns[_campaignId].donators,
+            campaigns[_campaignId].donations
+        );
+    }
+
+    function getCampaigns() public view returns (Campaign[] memory) {
+
+        //[{}, {}, {}]
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+        for (uint256 i = 0; i < numberOfCampaigns; i++) {
+            allCampaigns[i] = campaigns[i];
+        }
+        return allCampaigns;
+    }
 }
